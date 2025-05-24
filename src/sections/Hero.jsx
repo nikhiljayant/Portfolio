c67c95aefe35f7636fff87d7f5d6c42c14e05afc
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-05-25 00:12:51 +0530

    feat: enhance Hero component layout and structure

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+];
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative overflow-hidden" id="hero">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="Background" />
+      </div>
 
-export default Hero
+      <div className="hero-layout">
+        {/* LEFT: HERO CONTENT */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>Shaping</h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
+          </div>
+        </header>
+
+        {/* RIGHT: 3D MODEL */}
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
