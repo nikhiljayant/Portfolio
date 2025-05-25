71fd093584827f6bd2ac5c4fa47d30dfc258cbae
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-05-25 20:44:14 +0530

    feat: Add Hero section with 3D model and button component
    - Introduced a new Hero section that displays animated words with associated images.
    - Added a Button component for navigation.
    - Integrated HeroExperience component to showcase a 3D model of a room.
    - Updated package.json to include new dependencies: react-responsive and sharp.
    - Created constants for navigation links, words, and other data used in the application.
    - Implemented Room component to render the 3D model using GLTF.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
-const words = [
-  { text: "Ideas", imgPath: "/images/ideas.svg" },
-  { text: "Concepts", imgPath: "/images/concepts.svg" },
-];
+import Button from "../components/Button";
+import HeroExperience from "../components/heroModels/HeroExperience";
+
+import { words } from "../constants";
 
 const Hero = () => {
   return (
@@ -15,14 +15,49 @@ const Hero = () => {
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text">
-              <h1>Shaping</h1>
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words?.map((word) => (
+                      <span
+                        key={word?.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word?.imgPath}
+                          alt={word?.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word?.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
             </div>
+
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I'm Nikhil, a developer based in India with a passion for
+              code.
+            </p>
+
+            <Button
+              className={"md:w-80 md:h-16 w-60 h-12"}
+              id={"button"}
+              text={"See my Work"}
+            />
           </div>
         </header>
 
         {/* RIGHT: 3D MODEL */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   );
