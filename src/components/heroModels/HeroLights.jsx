f6017c9a6601405f2afa1b16e070ed00571314d8
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-05-28 23:53:49 +0530

    feat: update lighting positions in HeroLights component for improved scene illumination

--- src/components/heroModels/HeroLights.jsx
+++ src/components/heroModels/HeroLights.jsx
@@ -29,10 +29,14 @@ const HeroLights = () => {
 
       <primitive
         object={new THREE.RectAreaLight("#A259FF", 8, 3, 2)}
-        position={[0, 1, 0]}
+        position={[1, 3, 4]}
         intensity={15}
         rotation={[-Math.PI / 4, -Math.PI / 4, 0]}
       />
+
+      <pointLight position={[0, 1, 0]} intensity={10} color="#7209b7" />
+
+      <pointLight position={[1, 2, -2]} intensity={10} color="#0d00a4" />
     </>
   );
 };
