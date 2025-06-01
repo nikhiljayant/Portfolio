3c5b7698027b930af6d8a563df4d80a2418d2ad2
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-06-01 16:15:06 +0530

    feat: add ShowcaseSection component with GSAP animations for project display

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,80 @@
+import { useRef } from "react";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ShowcaseSection = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+
+    projects.forEach((project, index) => {
+      gsap.fromTo(
+        project,
+        {
+          y: 50,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: project,
+            start: "top bottom-=100",
+          },
+        }
+      );
+    });
+  });
+
+  return (
+    <section id="work" ref={sectionRef} className="app-showcase">
+      <div className="w-full">
+        <div className="showcaselayout">
+          {/* LEFT */}
+          <div className="first-project-wrapper" ref={project1Ref}>
+            <div className="image-wrapper">
+              <img src="/images/project1.png" alt="Project 1" />
+            </div>
+            <div className="text-content">
+              <h2>Project 1</h2>
+              <p className="text-white-50 md:text-xl">Project 1 Description</p>
+            </div>
+          </div>
+
+          {/* RIGHT */}
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={project2Ref}>
+              <div className="image-wrapper bg-[#ffefdb]">
+                <img src="/images/project2.png" alt="Project 2" />
+              </div>
+              <h2>Project 2</h2>
+            </div>
+
+            <div className="project" ref={project3Ref}>
+              <div className="image-wrapper bg-[#ffe7eb]">
+                <img src="/images/project3.png" alt="Project 3" />
+              </div>
+              <h2>Project 3</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ShowcaseSection;
