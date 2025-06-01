05957eea4c5f777f5785ecace14bfa6d3b8fbf53
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-06-02 01:22:36 +0530

    feat: implement Navbar component with scroll detection and navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Hero />
       <ShowcaseSection />
     </>
