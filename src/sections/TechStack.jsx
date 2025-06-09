b6d3f88091647779345471dddc9a427311ffdd39
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-06-09 23:44:32 +0530

    feat: implement TechStack component with TitleHeader integration

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,7 +1,18 @@
+import TitleHeader from "../components/TitleHeader";
+
 const TechStack = () => {
   return (
-    <div>TechStack</div>
-  )
-}
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title={"My Preferred Tech Stack"}
+          sub={"🤝 The Skills I Bring to the Table"}
+        />
+      </div>
 
-export default TechStack
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default TechStack;
